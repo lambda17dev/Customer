4bdbf6ff2e1aeda05a4f5f020aa4f4258d27e82b
hangukjin <dlalsk1@naver.com>
2020-05-13 16:05:07 +0900

    Update Order.java

--- src/main/java/local/Order.java
+++ src/main/java/local/Order.java
@@ -53,10 +53,7 @@ public class Order {
             // 결제 실패
             // 부하 테스트
             for (int i = 0; i < 10000; i++){
-                for (int j = 0; j < 10000; j++)
-                {
-                    System.out.println(i*j);
-                }
+                System.out.println(i);
             }
 
 
